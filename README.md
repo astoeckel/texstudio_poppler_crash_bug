d13684f8e175d155645a9ac720a210fb0f9a4d76
Andreas Stöckel <astoecke@uwaterloo.ca>
2021-02-24 22:22:13 -0500

    Add simple version

--- README.md
+++ README.md
@@ -8,7 +8,32 @@ See
 
 for more information.
 
-## Building and Running
+## Pure C++ version (texstudio_poppler_crash_bug_simple)
+
+To compile and run either follow the instructions for the Qt version below or simply execute the following
+```
+# Clone the repository
+git clone git@github.com:astoeckel/texstudio_poppler_crash_bug
+
+# Download some test data
+wget http://compneuro.uwaterloo.ca/files/publications/stoeckel.2020c.pdf
+
+# Compile the program
+g++ -O1 -g -o texstudio_poppler_crash_bug_simple main_simple.cpp -lpthread `pkg-config poppler poppler-cpp -cflags -libs`
+
+# Run
+./texstudio_poppler_crash_bug stoeckel.2020c.pdf
+```
+
+You should get an output similar to the following:
+```
+32% done...texstudio_poppler_crash_bug_simple: strtod_l.c:1497: ____strtod_l_internal: Assertion `numsize == 1 && n < d' failed.
+```
+
+
+## Qt version (texstudio_poppler_crash_bug)
+
+### Building and Running
 
 We provide both a `meson` build definition, as well as a `qmake` project file.
 To build with `meson` you'll need to install `meson` via `pip`, as well as `ninja`; then, run the following:
---
+++ main_simple.cpp
@@ -0,0 +1,83 @@
+// Compile using
+// g++ -o texstudio_poppler_crash_bug_simple main_simple.cpp -lpthread `pkg-config poppler poppler-cpp -cflags -libs`
+
+#include <poppler-document.h>
+#include <poppler-page.h>
+#include <poppler-page-renderer.h>
+
+#include <atomic>
+#include <chrono>
+#include <iostream>
+#include <memory>
+#include <mutex>
+#include <thread>
+#include <random>
+
+const int N_THREADS = 16;
+const int N_IT_TAR = 10000;
+
+struct State {
+	std::unique_ptr<poppler::document> doc;
+	std::mutex doc_mtx;
+	std::atomic<int> n_it;
+	int n_pages;
+
+	State(const char *filename)
+	    : doc(poppler::document::load_from_file(filename)), n_it(0)
+	{
+		if (!doc) {
+			throw std::runtime_error("Error loading the document!");
+		}
+		n_pages = doc->pages();
+	}
+};
+
+void thread_main(State &state)
+{
+	std::random_device dev;
+	std::default_random_engine re(dev());
+	std::uniform_int_distribution<int> dist(0, state.n_pages - 1);
+
+	while (state.n_it < N_IT_TAR) {
+		state.n_it++;
+		poppler::page *page = nullptr;
+		{
+			std::lock_guard<std::mutex> lock(state.doc_mtx);
+			page = state.doc->create_page(dist(re));
+			if (!page) {
+				throw std::runtime_error("Error creating page instance!");
+			}
+		}
+
+		poppler::page_renderer renderer;
+		renderer.render_page(page, 20.0, 20.0);
+
+		{
+			std::lock_guard<std::mutex> lock(state.doc_mtx);
+			delete page;
+		}
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2) {
+		std::cout << "Usage: " << argv[0] << " [PDF File]" << std::endl;
+		return 1;
+	}
+
+	State state(argv[1]);
+	std::vector<std::thread> threads;
+	for (size_t i = 0; i < N_THREADS; i++) {
+		threads.emplace_back(thread_main, std::ref(state));
+	}
+	while (state.n_it < N_IT_TAR) {
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		std::cerr << "\r" <<(((state.n_it + 1) * 100) / N_IT_TAR) << "% done..." << std::flush;
+	}
+	std::cerr << std::endl;
+	for (size_t i = 0; i < N_THREADS; i++) {
+		threads[i].join();
+	}
+	return 0;
+}
